8f4453605938cd3267a1003adecbb2dcff680e66
Buga <ismabk94@gmail.com>
2023-04-13 14:36:57 +0200

    añadiendo el javascript

--- bascula.js
+++ bascula.js
@@ -1,3 +1,21 @@
-function prueba(){
+let encenderMaquina = false;
+
+function encender(){
+    if(encenderMaquina === false){
+    console.log("Encendiendo");
+    document.getElementById('maquina').classList.add("encenderMaquina");
+    document.getElementById('btn-nuevo').classList.add("encenderMaquina");
+    encenderMaquina= true
+    }
+}
+function apagar(){
+    if(encenderMaquina === true){
+    console.log("Apagando");
+    document.getElementById('maquina').classList.add("apagarMaquina");
+    document.getElementById('btn-nuevo').classList.add("apagarMaquina");
+    encenderMaquina=false
+    }
+}
+function precio(){
     console.log("Hola");
 }
